350cb0954be59a265bceff5acde741030f4b0fd5
gg <gilbert.gruaz@eduvaud.ch>
2019-12-17 10:48:16 +0100

    Commit du test de connexion csharp à mysql

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuery = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblQuery
+            // 
+            this.lblQuery.AutoSize = true;
+            this.lblQuery.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuery.Location = new System.Drawing.Point(12, 9);
+            this.lblQuery.Name = "lblQuery";
+            this.lblQuery.Size = new System.Drawing.Size(142, 16);
+            this.lblQuery.TabIndex = 0;
+            this.lblQuery.Text = "Contenu de la requête:";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(90, 60);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(98, 28);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 119);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblQuery);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblQuery;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+/*
+ Auteur: Raphaël Vögeli
+ Date: 10.01.2018
+ Description: 
+ Ce programme se connecte dans une base de donnée locale il demande qu'un base de donnée "test" aie été crée au préalable
+ Le programme créer un table ajoute des nouvelles information puis les met à joue et les affiche dans le label prévu à cet effet  
+*/
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        //variable de connection à la base mySQL
+        MySqlConnection mySqlConnection = new MySqlConnection(@"server=localhost; database=test; uid=root; pwd=root;");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //déclarations des commandes
+            MySqlCommand select = new MySqlCommand("SELECT * FROM test WHERE id=1", mySqlConnection);
+            MySqlCommand insert = new MySqlCommand("INSERT INTO test (id, password) values (1, '.Etml-')", mySqlConnection);
+            MySqlCommand create = new MySqlCommand("CREATE TABLE test(id int, password text)", mySqlConnection);
+            MySqlCommand update = new MySqlCommand("UPDATE test SET password='.Etml-44' WHERE id=1", mySqlConnection);
+
+            //se connecte à la base de donnée
+            mySqlConnection.Open();
+
+            //crée les tables selon la commande create
+            create.ExecuteNonQuery();
+
+            //insére les valeurs
+            insert.ExecuteNonQuery();
+
+            //change la valeur dans la table de .Etml- à .Etml-44
+            update.ExecuteNonQuery();
+
+            //Met dans la variable reader le contenu de la requete select
+            MySqlDataReader reader = select.ExecuteReader();
+
+            //passe à travers les donnee de la requete (equivalent d'un fetch en php)
+            while (reader.Read())
+            {
+                //met à jours l'affichage
+                lblQuery.Text = "Contenu de la requête: " + Convert.ToString(reader["password"]);
+            }
+        }
+    }
+}
